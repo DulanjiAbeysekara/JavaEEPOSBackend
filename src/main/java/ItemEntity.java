e2eb1210109193ab61260cdb10575d0681a7b8c5
DulanjiAbeysekara <dulanjiabeysekara09@gmail.com>
2024-02-06 14:26:24 +0530

    first commit

---
+++ src/main/java/ItemEntity.java
@@ -0,0 +1,16 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class ItemEntity {
+    private String code;
+    private String description;
+    private int qtyOnHand;
+    private Double unitPrice;
+
+
+}
